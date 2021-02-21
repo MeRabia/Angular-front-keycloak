fe0c817ba4a8e0dc76e339bca88fbe57a6484ee6
Mehdi Rabia <memehdirabiame@gmail.com>
2021-02-21 12:16:58 +0100

    first commit

---
+++ src/app/service/keycloak-security.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { rejects } from 'assert';
+import {KeycloakInstance} from 'keycloak-js';
+
+declare var Keycloak:any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class KeycloakSecurityService {
+  public kc;
+
+  constructor() { }
+
+  // tslint:disable-next-line:typedef
+   init(){
+     return new Promise((resolve, rejects) =>{
+      console.log('show me');
+      this.kc=new Keycloak({
+        url:"http://localhost:8080/auth",
+        realm:"my-ecom-realm",
+        clientId:"angularProductApp"
+      });
+
+       this.kc.init({
+        //onLoad:'login-required'
+        onLoad:"check-sso"
+      }).then((authenticated)=>{
+        console.log(authenticated)
+        console.log(this.kc.token)
+        resolve({auth:authenticated,token:this.kc.token})
+      });
+      console.log(this.kc.token);
+     }).catch(err=>{
+       rejects(err);
+     })
+
+  }
+
+}
